13cba13940d2f8142bc2a1c4aec647039a4c27ab
YukiJuda111 <3229971508@qq.com>
2024-03-07 23:42:34 +0800

    Deploy doggie on sepolia

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintBasicNft is Script {
     string public constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run() external {
-        address mostRecentlyDeployed = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512; // need to update by our self
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        ); // need to update by our self
         mintNftOnContract(mostRecentlyDeployed);
     }
 
